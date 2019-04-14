84e6dfa6469b709611add250245792a44966b9e3
kenneth <kenneth.hu@hotmail.com>
2019-04-14 13:04:28 +0800

    initial
    initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+ using Nethereum.Signer;
+ using Nethereum.Signer.Crypto;
+ 
+namespace Signing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "This is the message!";
+            var privateKey = "b5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
+            var providedAddress = "0x12890D2cce102216644c59daE5baed380d84830c";
+    
+            var signer = new Nethereum.Signer.MessageSigner();
+            var signature = signer.HashAndSign(message, privateKey);
+            Console.WriteLine("signature: {0} " ,signature);
+            //verification
+            var address = signer.HashAndEcRecover(message, signature);
+            Console.WriteLine("\n address:  {0}" ,address);
+            Console.WriteLine("\n result :  {0}" ,address == providedAddress);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # NethereumMessageSigning
 Using Nethereum for message signing
+
+
+# Step :
+
+## 1. donwload source code from this repository
+
+## 2. restore package
+
+```
+    dotnet restore
+```
+
+## 3. execute dotnet command 
+
+```
+    dotnet run
+```
+
+## 4. result
+
+![result](pic/pic3.png)
